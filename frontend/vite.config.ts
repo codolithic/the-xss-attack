0840126b79c52783f36b6c9a2b9f8742578e83d1
Pratap Reddy <pratapreddy15@gmail.com>
2026-06-09 10:56:37 +1000

    adding CSP to vite config and index.html and commenting it for reference

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const policies = [
+  "default-src 'self'",
+  "script-src 'self' 'unsafe-inline' https://js.stripe.com",
+  "style-src 'unsafe-inline'",
+  "frame-src https://js.stripe.com",
+  "connect-src 'self' http://localhost:8000",
+  "frame-ancestors 'none'",
+];
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // server: {
+  //   headers: {
+  //     "content-security-policy": policies.join("; "),
+  //   },
+  // },
 });
